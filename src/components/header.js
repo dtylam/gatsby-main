5822bed4e91a5804d14b4c0088dfc995a3a7fd0b
dtylam <8455943+dtylam@users.noreply.github.com>
2020-08-08 16:40:12 +0800

    monzo icon added

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,18 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faTwitter, faInstagram, faSpotify, faPaypal } from "@fortawesome/free-brands-svg-icons"
+import {
+  faTwitter,
+  faInstagram,
+  faSpotify,
+  faPaypal,
+} from "@fortawesome/free-brands-svg-icons"
+import {
+  faMugHot
+}from "@fortawesome/free-solid-svg-icons"
+import MonzoIcon from "../images/monzoicon"
 
 const Header = ({ siteTitle }) => {
   const data = useStaticQuery(graphql`
@@ -11,11 +21,11 @@ const Header = ({ siteTitle }) => {
       site {
         siteMetadata {
           social {
-            twitter,
-            instagram,
-            spotify,
-            paypal,
-            monzo,
+            twitter
+            instagram
+            spotify
+            paypal
+            monzo
             buymeacoffee
           }
         }
@@ -24,40 +34,80 @@ const Header = ({ siteTitle }) => {
   `)
   const { social } = data.site.siteMetadata
   return (
-    <nav class="navbar is-fixed-top" role="navigation" style={{ background: `#00d1b2` }}>
-      <div
-        class="navbar-brand"
-        style={{
-          margin: `0 auto`,
-          padding: `1.45rem`,
-        }}
-      >
-        <h1>
-          <Link
-            to="/"
-            style={{
-              textDecoration: `none`,
-            }}
-          >
-            {siteTitle}
-          </Link>
-        </h1>
-      </div>
-      <div class="navbar-menu is-active">
-        <div div class="navbar-start">
-          <a class="navbar-item" href={`https://twitter.com/${social.twitter}`} target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faTwitter} />
-          </a>
-          <a class="navbar-item" href={`https://instagram.com/${social.instagram}`} target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faInstagram} />
-          </a>
-          <a class="navbar-item" href={`https://https://open.spotify.com/user/${social.spotify}`} target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faSpotify} />
-          </a>
-          <a class="navbar-item" href={`https://paypal.me/${social.paypal}`} target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faPaypal} />
-          </a>
+    <nav
+      class="navbar is-fixed-top"
+      role="navigation"
+      style={{ background: `#00d1b2` }}
+    >
+      <div class="level is-mobile" style={{display: `flex`}}>
+        <div
+          class="level-item has-text-centered"
+          style={{
+            margin: `0 auto`,
+            padding: `1.45rem`,
+          }}
+        >
+          <h1>
+            <Link
+              to="/"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+              }}
+            >
+              {siteTitle}
+            </Link>
+          </h1>
         </div>
+        <a
+          class="level-item"
+          style={{minHeight: `100%`, display: `flex`}}
+          href={`https://twitter.com/${social.twitter}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a
+          class="level-item"
+          href={`https://instagram.com/${social.instagram}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a
+          class="level-item"
+          href={`https://https://open.spotify.com/user/${social.spotify}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faSpotify} />
+        </a>
+        <a
+          class="level-item"
+          href={`https://paypal.me/${social.paypal}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faPaypal} />
+        </a>
+        <a
+          class="level-item"
+          href={`https://monzo.me/${social.monzo}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <MonzoIcon/>
+        </a>
+        <a
+          class="level-item"
+          href={`https://buymeacoff.ee/${social.buymeacoffee}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faMugHot} />
+        </a>
       </div>
     </nav>
   )
---
+++ src/images/monzoicon.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const MonzoIcon = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "monzo.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return <Img fluid={data.placeholderImage.childImageSharp.fluid} alt="Monzo logo" className="media-left image is-16x16"/>
+}
+
+export default MonzoIcon
